7c3f116ab3c501f1604798b012aea3477367a539
Cody Burenski <ctburenski@icloud.com>
2021-09-26 00:52:56 +0000

    Adding api endpoints and svelte frontend

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ import {
   prerenderedMiddleware,
   kitMiddleware,
 } from "./build/middlewares.js";
+import { api } from "./routes/api.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+app.options("*", cors());
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
+app.use("/api", api);
+
+app.use("/static", express.static("user-files"));
 
 app.use(assetsMiddleware, prerenderedMiddleware, kitMiddleware);
 
