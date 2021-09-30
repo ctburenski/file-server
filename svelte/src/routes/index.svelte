d9b0db7cdf5e503826018f4b5aca984157f920ae
Cody Burenski <ctburenski@icloud.com>
2021-09-30 23:42:59 +0000

    Removing all styling for now Switching from bulma to tailwind from scratch

--- svelte/src/routes/index.svelte
+++ svelte/src/routes/index.svelte
@@ -37,100 +37,40 @@
 	}
 </script>
 
-<div class="box">
-	<h1 class="title">Upload an Image or Video</h1>
-	<form on:submit|preventDefault={submitFile}>
-		<div class="field">
-			<div class="file is-boxed is-primary">
-				<label class="file-label">
-					<input class="file-input" type="file" name="user-file" bind:files={fileUpload} />
-					<span class="file-cta">
-						<span class="file-icon">
-							<i class="fas fa-file-upload" />
-						</span>
-						<span class="file-label"> Choose a file </span>
-					</span>
-					<span class="file-name">{fileUpload?.[0]?.name || 'Select a file'}</span>
-				</label>
-			</div>
-		</div>
-		{#if warnUploadFile}
-			<div class="field">
-				<div class="notification is-warning is-light">
-					Invalid file type - please only upload images or videos!
-				</div>
-			</div>
-		{/if}
-		<div class="field">
-			<div class="control">
-				<input class="button is-primary" type="submit" />
-			</div>
-		</div>
-	</form>
-</div>
+<h1>Upload an Image or Video</h1>
+<form on:submit|preventDefault={submitFile}>
+	<label>
+		<input type="file" name="user-file" bind:files={fileUpload} />
+		<span>
+			<i class="fas fa-file-upload" />
+		</span>
+		<span> Choose a file </span>
+		<span>{fileUpload?.[0]?.name || 'Select a file'}</span>
+	</label>
+	{#if warnUploadFile}
+		<p>Invalid file type - please only upload images or videos!</p>
+	{/if}
+	<button
+		type="submit"
+		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+	>
+		Submit
+	</button>
+</form>
 {#if fileData}
-	<div class="grid">
-		{#each fileData as file (file.fileDate)}
-			<div class="card">
-				<header class="card-header">
-					<p class="card-header-title">{file.fileName.slice(file.fileName.indexOf('-') + 1)}</p>
-				</header>
-				<div class="card-image">
-					{#if file.fileType === 'video'}
-						<div class="video-container">
-							<video controls>
-								<source src={`${baseUrl}/static/${file.fileName}`} type={file.fileMime} />
-								<track kind="captions" />
-							</video>
-						</div>
-					{:else if file.fileType === 'image'}
-						<div class="image-container">
-							<img
-								class="image"
-								src={`${baseUrl}/static/${file.fileName}`}
-								alt=""
-								type={file.fileMime}
-							/>
-						</div>
-					{/if}
-				</div>
-			</div>
-		{/each}
-	</div>
+	{#each fileData as file (file.fileDate)}
+		<header>
+			<p>{file.fileName.slice(file.fileName.indexOf('-') + 1)}</p>
+		</header>
+		{#if file.fileType === 'video'}
+			<video controls>
+				<source src={`${baseUrl}/static/${file.fileName}`} type={file.fileMime} />
+				<track kind="captions" />
+			</video>
+		{:else if file.fileType === 'image'}
+			<img src={`${baseUrl}/static/${file.fileName}`} alt="" type={file.fileMime} />
+		{/if}
+	{/each}
 {:else}
-	<div class="box">
-		<h1>Waiting to get files from server...</h1>
-		<progress class="progress is-large is-info" max="100">60%</progress>
-	</div>
+	<h1>Waiting to get files from server...</h1>
 {/if}
-
-<style>
-	.grid {
-		padding: 1rem;
-		gap: 1rem;
-		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
-	}
-	.video-container,
-	.image-container {
-		display: flex;
-		justify-content: center;
-		width: 100%;
-		padding-top: 75%;
-		position: relative;
-	}
-	video,
-	.image {
-		display: flex;
-		position: absolute;
-		top: 0;
-		height: 100%;
-	}
-	video {
-		width: 100%;
-	}
-	.image {
-		justify-content: center;
-		width: auto;
-	}
-</style>
